9f25fb804881ff90bfc6e3d1b546ef63a92f82e8
Alec Navapanich <alecnavapanich@gmail.com>
2025-11-05 20:38:36 -0800

    Initial commit

---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Anyone can cook.</Text>
+        </View>
+    )
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
